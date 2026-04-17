f8b213d5d20e151f7275577eb792fbb09386216f
red-slime <xaneslime@gmail.com>
2026-04-17 16:23:06 -0400

    update to blog structure

---
+++ src/pages/blog/posts.ts
@@ -0,0 +1,38 @@
+export const posts = [
+	{
+		slug: "the-death-of-corporate-whimsy",
+		title: "The Death of Corporate Whimsy",
+		tldr: "businesses forsake identity to achieve mass apeal.",
+		date: "2026-02-26",
+		displayDate: "February 26th, 2026",
+		author: "XAAANE",
+	},
+	{
+		slug: "rgb-enjoyer-goes-cmyk",
+		title: "RGB enjoyer goes CMYK for baby's first print",
+		tldr: "desiging IRL posters to promote myself was a fun letdown.",
+		date: "2026-02-26",
+		displayDate: "February 26th, 2026",
+		author: "XAAANE",
+	},
+	{
+		slug: "ai-fear-mongering",
+		title: "AI Fear Mongering",
+		tldr: "use AI to build your website, then hit me up when it can't.",
+		date: "2026-03-13",
+		displayDate: "March 13th, 2026",
+		author: "XAAANE",
+	},
+	{
+		slug: "linkedin-surveillance",
+		title: "LinkedIn Leverages its Monopoly for Mass Surveillance",
+		tldr: "they're running a massive, global, and illegal spying operation.",
+		date: "2026-04-16",
+		displayDate: "April 16th, 2026",
+		author: "XAAANE",
+	},
+];
+
+export function getPostBySlug(slug: string) {
+	return posts.find((post) => post.slug === slug);
+}
